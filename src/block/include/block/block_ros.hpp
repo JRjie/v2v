67628e59ff89ef62595732fcc64a3d803dbf1016
jie <1261536301@qq.com>
2025-01-04 22:14:51 +0800

    create head file

---
+++ src/block/include/block/block_ros.hpp
@@ -0,0 +1,12 @@
+#ifndef BLOCK_ROS_HPP
+#define BLOCK_ROS_HPP
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+#endif
--- src/block/src/block_ros.cpp
+++ src/block/src/block_ros.cpp
@@ -1,9 +1,4 @@
-#include <chrono>
-#include <memory>
-#include <string>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "block/block_ros.hpp"
 
 int main()
 {
