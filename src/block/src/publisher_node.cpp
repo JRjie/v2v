75464a5d2108895fa323ed9a94f707c81399aeba
jie <1261536301@qq.com>
2025-03-04 23:01:09 +0800

    add udp

--- src/block/src/publisher_node.cpp
+++ src/block/src/publisher_node.cpp
@@ -1,11 +1,39 @@
 #ifndef PUBLISHER_NODE_CPP
 #define PUBLISHER_NODE_CPP
 
+#include "block/publisher_node.hpp"
 namespace block
 {
 
-#include "block/publisher_node.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+PublisherNode::PublisherNode(const rclcpp::NodeOptions &options)
+: rclcpp::Node("publisher_node", options)
+{
+  this->declare_parameter("port", 12345);
+  port = this->get_parameter("port").as_int();
+
+  publisher = this->create_publisher<my_interfaces::msg::PostureArray>("posture_array", rclcpp::QoS(10));
+  
+  socket = std::make_unique<boost::asio::ip::udp::socket>(io_service, udp::endpoint(udp::v4(), port));
+
+  udp_thread = std::thread([this]() {
+    CreateUDP();
+    io_service.run();
+  });
+}
+
+PublisherNode::~PublisherNode()
+{
+  io_service.stop();
+  if(upd_thread.joinable())
+  {
+    udp_thread.join();
+  }
+}
+
+void PublisherNode::CreateUDP(const io_service &io, const int &port)
+{
+
+}
 
 } //namespace
 #include "rclcpp_components/register_node_macro.hpp"
