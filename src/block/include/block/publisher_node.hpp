d37f023f88a1fadbb477e1ff60f9eacbe2568887
jie <1261536301@qq.com>
2025-03-04 23:01:17 +0800

    add udp

--- src/block/include/block/publisher_node.hpp
+++ src/block/include/block/publisher_node.hpp
@@ -5,8 +5,12 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <thread>
+#include <memory>
 
 #include <rclcpp/rclcpp.hpp>
+
+#include <boost/asio.hpp>
 
 #include "my_interface/posture_array.hpp"
 
@@ -15,10 +19,18 @@ namespace block
 class PublisherNode : public rclcpp::Node {
   public:
     explicit PublisherNode(const rclcpp::NodeOptions & options);
+    explicit ~PublisherNode();
 
   private:
     rclcpp::Publisher<my_interfaces::msg::PostureArray>::SharedPtr publisher;
+    void CreateUDP(const io_service &io, const int &port);
 
+  private:
+    int port;
+    io_service io_service;
+    std::vector<char> receive_buffer;
+    std::unique_ptr<boost::asio::ip::udp::socket> socket;
+    std::thread udp_thread;
 
 };
 
